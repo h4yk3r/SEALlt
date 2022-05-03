bd4534533d086fcd02c0a1c7cfcee863966d9950
rp1213 <ayhuweber@foxmail.com>
2022-05-04 00:46:23 +0800

    update test for d-lt

--- native/ORtest/main.cpp
+++ native/ORtest/main.cpp
@@ -40,18 +40,35 @@ int main ()
     seal::Decryptor decryptor(context, secret_key);
 
     vector<double> V;
-    for (int i=0; i!=1024; ++i) V.push_back(i + 1.0);
+    for (int i=0; i!=4; ++i) V.push_back(i + 1.0);
+    vector<vector<double>> M;
+    for (int i=0; i!=4; ++i)
+    {
+        vector<double> temp;
+        for (int j=0; j!=4; ++j) temp.push_back(j + 1.0);
+        M.push_back(temp);
+    }
     seal::Plaintext encodedV;
     ckks_encoder.paddingEncode(V, scale, encodedV);
     seal::Ciphertext encV;
     encryptor.encrypt(encodedV, encV);
+    vector<seal::Plaintext> encodedM;
+    for (int i=0; i!=4; ++i)
+    {
+        seal::Plaintext temp;
+        ckks_encoder.paddingEncode(M[i], scale, temp);
+        encodedM.push_back(temp);
+    }
+    seal::Ciphertext encres;
+    evaluator.linear_transformation(encV, encodedM, 4, 4, galois_keys, relin_keys, encres);
+    // evaluator.rotate_vector(encV, 1024, galois_keys, encres);
     seal::Plaintext decV;
-    decryptor.decrypt(encV, decV);
+    decryptor.decrypt(encres, decV);
     vector<double> evaV;
     ckks_encoder.decode(decV, evaV);
 
-    cout << "[ ";
-    for (auto i : evaV) printf("%.3lf ", i);
+    cout << " [ ";
+    for (int i=0; i!=4; ++i) printf("%.3lf ", evaV[i*1024]);
     cout << "]" << endl;
 
     return 0;

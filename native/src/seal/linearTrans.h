d0afc223490fbd241a7907697be3a504835a35f5
rp1213 <ayhuweber@foxmail.com>
2022-05-15 13:26:35 +0000

    complete LT

--- native/src/seal/linearTrans.h
+++ native/src/seal/linearTrans.h
@@ -90,42 +90,130 @@ namespace seal
 
     // main LT function
     seal::Ciphertext lt(
+            int method;
             seal::Ciphertext v, std::vector<std::vector<double>> &M, int height, int width,
             seal::CKKSEncoder &ckks_encoder, const double &scale,
             seal::Encryptor &encryptor, seal::Evaluator &evaluator,
             seal::GaloisKeys &galois_keys, seal::RelinKeys &relin__keys)
     {
-        seal::Ciphertext 
-        int method = 0;
-        if (height == width) method = 1; // square matrices
-        else if (height < width) method = 2; // wide matrices
-        else if (height > width) method = 3; // tall matrices
+        seal::Ciphertext encryptedu = v;
 
         int idx = 0;
-        if (method == 1)
+        if (method == 1 || method == 2) // decomposing matrix & hybrid
         {
-            // squre matirces
+            int stride = ckks_encoder.slot_count() / v.size();
+            std::vector<seal::Ciphertext> rotedu(height);
+            for (idx = 0; idx < height; ++idx)
+			{
+				evaluator.rotate_vector(encryptedu, idx * stride, galois_keys, rotedu[idx]);
+			}
             std::vector<std::vector<double>> tiledM = tillingMatrix(M, height, width);
-            for (idx = 0; idx != height; ++idx)
-            {
-                seal::Ciphertext temp;
-                evaluator.multiply_plain();
-            }           
-
-
-            return v;
+            std::vector<seal::Plaintext> encodedM;
+			for (auto v : tiledM)
+			{
+				seal::Plaintext temp;
+				ntEncoding(ckks_encoder, scale, v, temp);
+				encodedM.push_back(temp);
+			}
+            seal::Ciphertext enc_result;
+			evaluator.multiply_plain(rotedu[0], encodedM[0], enc_result);
+			evaluator.relinearize_inplace(enc_result, relin_keys);
+			evaluator.rescale_to_next_inplace(enc_result);
+			for (idx = 1; idx < height; ++idx)
+			{
+				seal::Ciphertext temp;
+				evaluator.multiply_plain(rotedu[idx], encodedM[idx], temp);
+				evaluator.relinearize_inplace(temp, relin_keys);
+				evaluator.rescale_to_next_inplace(temp);
+				evaluator.add_inplace(enc_result, temp);
+			}         
+            if (method == 2) // additional R&S
+			{
+				int num = ckks_encoder.slot_count();
+				while (num != height * stride)
+				{
+					Ciphertext temp;
+					evaluator.rotate_vector(enc_result, num / 2, galois_keys, temp);
+					evaluator.add_inplace(enc_result, temp);
+					num /= 2;
+				}
+			}
+            return enc_result;
         }
-        else if (method == 2)
+        else if (method == 3) // bsgs
         {
-            // wide matrices
-
-            return v;
-        }
-        else if (method == 3)
-        {
-            // tall matrices
-
-            return v;
+            // BSGS parameters
+			int max_len = std::max(height, width);
+			int min_len = std::min(height, width);
+			double n_ddot = (double)min_len;
+			int g_tilde = (int)ceil(sqrt(n_ddot));
+			int b_tilde = (int)ceil(n_ddot / g_tilde);
+			// preprocessing matrix
+			std::vector<seal::Plaintext> encodedM(min_len);
+			std::vector<std::vector<double>> tiledM = tilingMatrix(M, height, width);
+			for (int b = 0; b < b_tilde; ++b)
+			{
+				for (int g = 0; g < g_tilde && b * g_tilde + g < min_len; ++g)
+				{
+					std::vector<double>& temp = tiledM[b * g_tilde + g];
+					std::rotate(temp.rbegin(), temp.rbegin() + b * g_tilde, temp.rend());
+					Plaintext coded_temp;
+					ntEncoding(ckks_encoder, scale, temp, coded_temp);
+					encodedM[b * g_tilde + g] = coded_temp;
+				}
+			}
+			encodedM.shrink_to_fit();
+			// rotate ciphertext
+			vector<seal::Ciphertext> rotedu(g_tilde);
+			rotedu[0] = encryptedu;
+			for (idx = 1; idx < g_tilde; ++idx)
+			{
+				evaluator.rotate_vector(encryptedu, idx * stride, galois_keys, rotedu[idx]);
+			}
+			// evaluate
+			Ciphertext res;
+			int g, b;
+			// b=0, g=0
+			evaluator.multiply_plain(rotedu[0], encodedM[0], res);
+			evaluator.relinearize_inplace(res, relin_keys);
+			evaluator.rescale_to_next_inplace(res);
+			// b=0, g>=1
+			for (g = 1; g < g_tilde; ++g)
+			{
+				Ciphertext temp = rotedu[g];
+				evaluator.multiply_plain_inplace(temp, encodedM[g]);
+				evaluator.relinearize_inplace(temp, relin_keys);
+				evaluator.rescale_to_next_inplace(temp);
+				evaluator.add_inplace(res, temp);
+			}
+			//b>=1
+			for (b = 1; b < b_tilde; ++b)
+			{
+				Ciphertext temp_res;
+				for (size_t g = 0; g != g_tilde && b * g_tilde + g != min_len; ++g)
+				{
+					Ciphertext temp = rotedu[g];
+					evaluator.multiply_plain_inplace(temp, encodedM[b * g_tilde + g]);
+					evaluator.relinearize_inplace(temp, relin_keys);
+					evaluator.rescale_to_next_inplace(temp);
+					if (g == 0) temp_res = temp;
+					else evaluator.add_inplace(temp_res, temp);
+				}
+				evaluator.rotate_vector_inplace(temp_res, b * g_tilde * stride, galois_keys);
+				evaluator.add_inplace(res, temp_res);
+			}
+			if (height < width) // additional R&S
+			{
+				int num = width / height;
+				while (num != 1)
+				{
+					Ciphertext temp = res;
+					evaluator.rotate_vector_inplace(res, height * num * stride / 2, galois_keys);
+					evaluator.add_inplace(res, temp);
+					num /= 2;
+				}
+			}
+            return res;
         }
     }
 
